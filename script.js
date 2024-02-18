5ba73679f5aa708ed0343b63368c789019c7c8b7
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2024-02-18 00:37:10 -0500

    animated add book div

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnAdd = document.getElementById("btnAddBook");
+const addBox = document.querySelector(".add");
+
+btnAdd.addEventListener("click", e => {
+    btnAdd.classList.toggle("addmode");
+    if (btnAdd.classList.contains("addmode")) {
+        addBox.classList.add("addmode");
+    } else {
+        addBox.classList.remove("addmode");
+    }
+});

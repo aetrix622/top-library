af194ac46d79020907546488fb2f9869bfeea7bd
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2024-02-18 01:32:47 -0500

    add new book submission buttons

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const btnAdd = document.getElementById("btnAddBook");
 const addBox = document.querySelector(".add");
+const btnSubmitNewBook = document.getElementById("addbutton");
+const btnCancelNewBook = document.getElementById("cancelbutton");
+const txtNewTitle = document.getElementById("newtitle");
+const txtNewAuthor = document.getElementById("newauthor");
+const txtNewPages = document.getElementById("newpages");
+const rdoNewRead = document.getElementById("newread");
 
 btnAdd.addEventListener("click", e => {
     btnAdd.classList.toggle("addmode");
@@ -8,4 +14,19 @@ btnAdd.addEventListener("click", e => {
     } else {
         addBox.classList.remove("addmode");
     }
+});
+
+btnSubmitNewBook.addEventListener("click", e => {
+    e.preventDefault();
+});
+
+btnCancelNewBook.addEventListener("click", e => {
+    e.preventDefault();
+    // clear all form fields then hide the form
+    txtNewTitle.textContent = "";
+    txtNewAuthor.textContent = "";
+    txtNewPages.textContent = "";
+    rdoNewRead.checked = true;
+    btnAdd.classList.remove("addmode");
+    addBox.classList.remove("addmode");
 });

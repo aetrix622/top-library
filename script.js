b76b0e288fa0fa1b6082f48bf533ce5f6fd70f4b
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2024-02-21 20:27:48 -0500

    removed hardcoded HTML book cards

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const txtNewTitle = document.getElementById("newtitle");
 const txtNewAuthor = document.getElementById("newauthor");
 const txtNewPages = document.getElementById("newpages");
 const rdoNewRead = document.getElementById("newread");
+const bookListDiv = document.querySelector(".list");
 
 btnAdd.addEventListener("click", e => {
     btnAdd.classList.toggle("addmode");
@@ -41,16 +42,16 @@ const Book = function(title, author, pages, read) {
 };
 
 Book.prototype.generateCardHTML = function() {
-    let htmlBlock = `
-        <div class="card">
-            <div class="cover"><img src="${this.coverArt} alt="${this.coverArtAlt}" class="default"></div>
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    cardDiv.innerHTML = `
+            <div class="cover"><img src="${this.coverArt}" alt="${this.coverArtAlt}" class="default"></div>
             <div>Title</div><div>${this.title}</div>
             <div>Author</div><div>${this.author}</div>
             <div>Pages</div><div>${this.pages}</div>
             <div class="readstatus">${this.read ? "Read" : "Not Read"}</div>
-        </div>
     `;
-    return htmlBlock;
+    return cardDiv;
 };
 
 var books = [];
@@ -58,4 +59,10 @@ var books = [];
 books.push(new Book("The Adventures of Tom Sawyer", "Samuel Clemens", 432, true));
 books.push(new Book("The Hobbit", "J.R.R. Tolkien", 1024, false));
 books.push(new Book("The Road", "Cormack McCarthy", 837, true));
+
+document.addEventListener("DOMContentLoaded", e => {
+    for (let book of books) {
+        bookListDiv.appendChild(book.generateCardHTML());
+    }
+});
 

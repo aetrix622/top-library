d0f685ed282bd6a19c66a3a183469f99a9f6e138
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2024-02-23 12:16:29 -0500

    split name into first and last name components and sorted by last

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const addBox = document.querySelector(".add");
 const btnSubmitNewBook = document.getElementById("addbutton");
 const btnCancelNewBook = document.getElementById("cancelbutton");
 const txtNewTitle = document.getElementById("newtitle");
-const txtNewAuthor = document.getElementById("newauthor");
+const txtNewAuthorLastName = document.getElementById("lastname");
+const txtNewAuthorFirstName = document.getElementById("firstname");
 const txtNewPages = document.getElementById("newpages");
 const rdoNewRead = document.getElementById("newread");
 const rdoNewNotRead = document.getElementById("newnotread");
@@ -21,10 +22,11 @@ btnAdd.addEventListener("click", e => {
 btnSubmitNewBook.addEventListener("click", e => {
     e.preventDefault();
     let title = txtNewTitle.value;
-    let author = txtNewAuthor.value;
+    let lastName = txtNewAuthorLastName.value;
+    let firstName = txtNewAuthorFirstName.value;
     let pages = txtNewPages.value;
     let read = rdoNewRead.checked;
-    let book = new Book(title, author, pages, read);
+    let book = new Book(title, lastName, firstName, pages, read);
     books.push(book); 
     generateBookList();
     resetAddDialog();
@@ -37,8 +39,10 @@ btnCancelNewBook.addEventListener("click", e => {
 });
 
 const resetAddDialog = function() {
+    // hides the add a book dialog and blanks all fields
     txtNewTitle.value = "";
-    txtNewAuthor.value = "";
+    txtNewAuthorFirstName.value = "";
+    txtNewAuthorLastName.value = "";
     txtNewPages.value = "";
     rdoNewRead.checked = false;
     rdoNewNotRead.checked = true;
@@ -46,13 +50,20 @@ const resetAddDialog = function() {
     addBox.classList.remove("addmode");
 }
 
-const Book = function(title, author, pages, read) {
+const Book = function(title, lastName, firstName, pages, read) {
     this.title = title;
-    this.author = author;
+    this.author = {
+        firstName: firstName,
+        lastName: lastName,
+    }
     this.pages = pages;
     this.read = read;
     this.coverArt = "res/book-open-variant-outline.svg";
     this.coverArtAlt = "default cover"
+};
+
+Book.prototype.name = function() {
+    return this.author.firstName + " " + this.author.lastName;
 };
 
 Book.prototype.generateCardHTML = function(divIndex) {
@@ -61,7 +72,7 @@ Book.prototype.generateCardHTML = function(divIndex) {
     cardDiv.innerHTML = `
             <div class="cover"><img src="${this.coverArt}" alt="${this.coverArtAlt}" class="default"></div>
             <div>Title</div><div>${this.title}</div>
-            <div>Author</div><div>${this.author}</div>
+            <div>Author</div><div>${this.author.lastName}, ${this.author.firstName}</div>
             <div>Pages</div><div>${this.pages}</div>
             <div class="readstatus">${this.read ? "Read" : "Not Read"}</div>
             <div class="buttons">
@@ -74,9 +85,10 @@ Book.prototype.generateCardHTML = function(divIndex) {
 
 var books = [];
 // load some default books
-books.push(new Book("The Adventures of Tom Sawyer", "Samuel Clemens", 432, true));
-books.push(new Book("The Hobbit", "J.R.R. Tolkien", 1024, false));
-books.push(new Book("The Road", "Cormack McCarthy", 837, true));
+books.push(new Book("The Adventures of Tom Sawyer", "Twain", "Mark", 192, true));
+books.push(new Book("The Hobbit", "Tolkien", "J.R.R.", 300, true));
+books.push(new Book("The Road", "McCarthy", "Cormack", 837, true));
+books.push(new Book("Fahrenheit 451", "Bradbury", "Ray", 249, true));
 
 document.addEventListener("DOMContentLoaded", e => {
     generateBookList();
@@ -89,6 +101,13 @@ function generateBookList() {
         errorMessage.textContent = "Your Library is empty. Please add a book.";
         bookListDiv.appendChild(errorMessage);
     } else {
+        // sort by last name
+        books.sort((a,b) => {
+            let lastA = a.author.lastName;
+            let lastB = b.author.lastName;
+            return (lastA < lastB) ? -1 : 1;
+        });
+
         let index = 0;
         for (let book of books) {
             // add the card to the page
@@ -100,7 +119,9 @@ function generateBookList() {
         for (let toggleButton of toggleButtons) {
             toggleButton.addEventListener("click", e => {
                 let buttonClasses = Array.from(e.target.classList);
+                // remove toggleread class. last remaining class will be 'bookxxx'
                 buttonClasses.splice(buttonClasses.indexOf("toggleread"),1);
+                // remove the 'book' part to leave the number
                 cardIndex = buttonClasses[0].slice(4);
                 books[cardIndex].read = !books[cardIndex].read;
                 generateBookList();
